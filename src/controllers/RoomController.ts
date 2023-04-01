4a06a18325dc076ddfe4624e4d0c9632bf475526
viniciuzrock <vinir.santoss@gmail.com>
2023-03-31 21:04:27 -0300

    Concluindo projeto

--- src/controllers/RoomController.ts
+++ src/controllers/RoomController.ts
@@ -1,8 +1,10 @@
-import { Request, Response } from "express";
+import { Request, RequestHandler, Response } from "express";
 import { roomRepository } from "../repositories/roomRepository";
+import { videoRepository } from "../repositories/videoRepository";
+import { subjectRepository } from "../repositories/subjectRepository";
 
 export class RoomController{
-    async create(req: Request, res:Response){
+    async create(req: Request, res: Response){
         const {name, description} = req.body;
 
         try {
@@ -15,7 +17,70 @@ export class RoomController{
                 message: 'Internal server error'
             })
         }
+    }
 
+    async createVideo(req: Request, res: Response){
+        const { title, url } = req.body;
+        const {idRoom} = req.params;
 
+        try {                              //where
+            const room = await roomRepository.findOneBy({id:Number(idRoom)})
+
+            if(!room){
+                return res.status(404).json({
+                    message:'Success'
+                })
+            }
+            const newVideo = videoRepository.create({
+                title,
+                url,
+                room
+            });
+
+            await videoRepository.save(newVideo);
+            return res.status(201).json(newVideo)
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({
+                message: 'Internal server error'
+            })
+        }
+    };
+
+    async roomSubject(req: Request, res: Response){
+        const { subject_id } = req.body;
+        const { idRoom } = req.params;
+
+        try {
+            const room = await roomRepository.findOneBy({ id: Number(idRoom)})
+
+            if (!room){
+                return res.status(404).json({
+                    message: 'Aula não existe'
+                })
+            }
+
+            const subject = await subjectRepository.findOneBy({id: Number(subject_id)})
+
+            if (!subject){
+                return res.status(404).json({
+                    message: 'Disciplina não existe'
+                })
+            }
+
+            const roomUpdate = {
+                    ...room,
+                    subjects: [subject]
+                }
+
+            await roomRepository.save(roomUpdate)
+
+            return res.status(201).json(room)
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({
+                message: 'Internal server error'
+            })
+        }
     }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,9 @@ import { RoomController } from "./controllers/RoomController";
 
 const routes = Router();
 
-routes.post('/subject', new SubjectController().create)
-routes.post('/room', new RoomController().create)
-export default routes
+routes.post('/subject', new SubjectController().create);
+routes.post('/room', new RoomController().create);
+routes.post('/room/:idRoom/create', new RoomController().createVideo);
+routes.post('/room/:idRoom/subject', new RoomController().roomSubject);
+
+export default routes;
